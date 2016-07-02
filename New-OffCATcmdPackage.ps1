5092b1506b13872b21ac35beaddcc649fd131aff
Wojciech Sciesinski <wojciech@sciesinski.net>
2016-07-02 13:32:07 +0200

    0.1.0 - 2016-07-02 - The first draft

---
+++ New-OffCATcmdPackage.ps1
@@ -0,0 +1,57 @@
+﻿
+<#
+    .SYNOPSIS
+        Function intended to create OffCATcmd - OffCAT portable - package
+    
+    .DESCRIPTION
+        A detailed description of the New-OffCATcmdPackage function.
+    
+    .PARAMETER Path
+        A description of the Path parameter.
+    
+    .PARAMETER Source
+        A description of the Source parameter.
+    
+    .EXAMPLE
+        PS C:\> New-OffCATcmdPackage -  Path 'Value1' --Source 'Value2'
+    
+    .NOTES
+        
+    
+    .NOTES
+        Additional information about the function.
+    
+    .LINK
+    https://github.com/it-praktyk/
+
+    .LINK
+    https://www.linkedin.com/in/sciesinskiwojciech
+    
+    .NOTES
+    AUTHOR: Wojciech Sciesinski, wojciech[at]sciesinski[dot]net
+    KEYWORDS: PowerShell, <SOMETHING>
+    
+    VERSIONS HISTORY
+    - 0.1.0 - 2016-07-02
+    
+    TODO
+    
+    LICENSE  
+    Copyright (c) 2016 Wojciech Sciesinski  
+    This function is licensed under The MIT License (MIT)  
+    Full license text: https://opensource.org/licenses/MIT  
+    
+    #>
+
+#>
+function New-OffCATcmdPackage
+{
+    [CmdletBinding()]
+    param
+    (
+        $Path,
+        $Source
+    )
+    
+}
+
