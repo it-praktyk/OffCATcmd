08568634e4e9ddeaef375d1ed9c102a0527b3ce0
Wojciech Sciesinski <wojciech@sciesinski.net>
2016-07-02 13:26:44 +0200

    Module skeleton files added - using Plaster 0.1.0

---
+++ Tests/OffCATcmd.Tests.ps1
@@ -0,0 +1,11 @@
+﻿$ModuleManifestName = 'OffCATcmd.psd1'
+# 1e7a33f5-d982-482f-822d-dd288512324a - testing use of PLASTER predefined variables.
+Import-Module $PSScriptRoot\..\$ModuleManifestName
+
+Describe 'Module Manifest Tests' {
+    It 'Passes Test-ModuleManifest' {
+        Test-ModuleManifest -Path $PSScriptRoot\..\$ModuleManifestName
+        $? | Should Be $true
+    }
+}
+
